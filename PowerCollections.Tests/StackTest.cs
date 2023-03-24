3cde82c0c74c654a76ab5e7e5bf35cfaae46b08f
Kuznecovn447 <nikita4862@list.ru>
2023-03-24 15:09:39 +0400

    initial commit

---
+++ PowerCollections.Tests/StackTest.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestPlatform.CommunicationUtilities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PowerCollections.Tests
+{
+    [TestClass]
+    public class StackTests
+    {
+        [TestMethod]
+        public void stack_with_request_capacity_and_null_count_element()
+        {
+            Stack<int> stack = new Stack<int>(18);
+            Assert.AreEqual(18, stack.Capacity);
+            Assert.AreEqual(0, stack.Count);
+        }
+
+        [TestMethod]
+        public void count_stack_after_add_element()
+        {
+            Stack<string> stack = new Stack<string>(4);
+            stack.Push("Элемент 1");
+            Assert.AreEqual(1, stack.Count);
+        }
+
+        [TestMethod]
+        public void adding_an_array_to_the_stack()
+        {
+            string[] str = new string[] { "Nikita", "Denis", "Kirill" };
+            Stack<string> stack = new Stack<string>(str, 10);
+            Assert.AreEqual(3, stack.Count);
+        }
+
+        [TestMethod]
+        public void return_top_element_from_stack()
+        {
+            Stack<int> stack = new Stack<int>(2);
+            stack.Push(1);
+            stack.Push(2);
+            int result = stack.Top();
+            Assert.AreEqual(2, result);
+        }
+
+        [TestMethod]
+        public void return_and_delete_top_element_from_stack()
+        {
+            Stack<string> stack = new Stack<string>(3);
+            stack.Push("Denis");
+            stack.Push("Nikita");
+            string result = stack.Pop();
+            Assert.AreEqual("Nikita", result);
+            Assert.AreEqual(1, stack.Count);
+        }
+
+        [TestMethod]
+        public void returning_a_clean_stack()
+        {
+            string[] str = new string[] { "Nikita", "Denis", "Kirill" };
+            Stack<string> stack = new Stack<string>(str, 10);
+            stack.Clear();
+            Assert.AreEqual(0, stack.Count);
+        }
+
+        [TestMethod]
+        public void checking_interface_enumerable_topdown()
+        {
+            int[] numbers = new int[] { 1, 2, 3 };
+            Stack<int> stack = new Stack<int>(numbers, 10);
+
+            int i = 1;
+            foreach (int el in stack)
+            {
+                Assert.AreEqual(i++, el);
+            }
+        }        
+
+        [TestMethod]
+        public void set_MaxSize_stack_ArrayLength_if_ArrayLength_more_StackLength()
+        {
+            string[] str = new string[] { "Nikita", "Denis", "Kirill" };
+            Stack<string> stack = new Stack<string>(str, 1);
+            Assert.AreEqual(3, stack.Count);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void call_exception_if_stack_overflow()
+        {
+            Stack<string> stack = new Stack<string>(1);
+            stack.Push("S");
+            stack.Push("D");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(IndexOutOfRangeException))]
+        public void raise_an_exception_when_the_stack_is_empty()
+        {
+            Stack<string> stack = new Stack<string>(0);
+            stack.Pop();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void call_exception_if_do_TOP_empty_stack()
+        {
+            Stack<string> stack = new Stack<string>(0);
+            stack.Top();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void exception_if_MaxSize_stack_less_Null()
+        {
+            Stack<string> stack = new Stack<string>(-1);
+        }
+    }
+}
---
+++ PowerCollections/Stack.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PowerCollections
+{
+    internal class Stack<T>
+    {
+        private T[] stack;
+        private int count;
+        private int capacity;
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = stack.Length - 1; i > -1 ; i--)
+            {
+                yield return stack[i];
+            }
+        }
+
+        public Stack(int maxSize)
+        {
+            capacity = maxSize;
+            stack = new T[capacity];
+        }
+
+        public Stack(T[] array, int maxSize)
+        {
+            if(array.Length > maxSize)
+                capacity = array.Length;
+            else
+                capacity = maxSize;
+            stack = array;
+            count = stack.Length; 
+        }
+
+        public int Count 
+        {
+            get { return count; } 
+        }
+
+        public int Capacity 
+        {
+            get { return capacity; }
+        }
+
+        public void Push(T element)
+        {
+            stack[count] = element;
+            count++;
+        }
+
+        public T Top()
+        {
+            T element = stack[count - 1];
+            return element;
+        }
+
+        public T Pop()
+        {
+            T element = stack[count - 1];
+            Array.Copy(stack, stack, count - 1);
+            count--;
+            return element;
+        }
+
+        public void Clear()
+        {
+            count = 0;
+            stack = new T[capacity];
+        }
+
+    }
+}
